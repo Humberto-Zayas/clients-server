6672f81eaacc0b5622fadcd88a11a972c3aa11bf
Humberto Zayas <soundsinhd@gmail.com>
2023-05-05 22:49:31 -0400

    updated server to allow login of a general user and change a title

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const HeroTitle = require('./models/heroTitle');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const router = express.Router();
+const session = require('express-session');
 
 const app = express();
 const port = 5001;
+
+// Static credentials
+const ADMIN_USERNAME = 'admin';
+const ADMIN_PASSWORD = 'password';
+
+// Set up Passport for authentication
+passport.use(new LocalStrategy(async (username, password, done) => {
+  if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+    return done(null, { username: ADMIN_USERNAME });
+  }
+  return done(null, false, { message: 'Incorrect username or password' });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.username);
+});
+
+passport.deserializeUser(async (username, done) => {
+  if (username === ADMIN_USERNAME) {
+    return done(null, { username: ADMIN_USERNAME });
+  }
+  return done(new Error('User not found'));
+});
 
 // Connect to the MongoDB server
 mongoose.connect('mongodb://localhost/myapp', {
@@ -21,28 +49,44 @@ mongoose.connect('mongodb://localhost/myapp', {
 app.use(bodyParser.json());
 
 // Enable CORS for all routes
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace this with your React app's URL
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
-// Define a route to handle POST requests to change the hero title
-app.post('/hero-title/:website', async (req, res) => {
-  const website = req.params.website;
-  const newTitle = req.body.title;
+// Set up session middleware
+app.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if you are using HTTPS
+}));
+
+// Initialize Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Login route
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json({ isAuthenticated: true });
+});
+
+// Route to update the hero title for a website
+app.post('/hero-title/:website', passport.authenticate('local', { session: false }), async (req, res) => {
+  const { website } = req.params;
+  const { title } = req.body;
 
   try {
-    let heroTitle = await HeroTitle.findOne({ website: website });
-
+    const heroTitle = await HeroTitle.findOneAndUpdate({ website }, { title }, { new: true });
     if (!heroTitle) {
-      heroTitle = new HeroTitle({ website: website, title: newTitle });
-    } else {
-      heroTitle.title = newTitle;
+      return res.status(404).json({ error: `Hero title not found for website '${website}'` });
     }
-
-    await heroTitle.save();
-
-    return res.status(200).send(`Hero title for '${website}' updated to '${newTitle}'`);
+    res.json(heroTitle);
   } catch (err) {
     console.error('Error updating hero title:', err);
-    return res.status(500).send(err);
+    res.status(500).json({ error: 'Error updating hero title' });
   }
 });
 
